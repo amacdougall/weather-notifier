4c597305c1539471af411756e8f53f75ae91a775
amacdougall <smoke@alanmacdougall.com>
2011-09-11 18:57:12 -0500

    Added debug behavior for temperature retrieval and updates.

--- notifier.py
+++ notifier.py
@@ -6,18 +6,31 @@ import xml.dom.minidom
 from datetime import datetime
 from urllib import urlopen
 
+# general configuration
+DEBUG = True
+
 # email configuration
 SENDER = "smoke@alanmacdougall.com"
 RECIPIENTS = ("smoke@alanmacdougall.com", "nigel.wade7@gmail.com")
 SUBJECT = "Weather Update"
-BODY = """The temperature outside is now %d, which is within the target range
-for MAXIMUM ENJOYMENT. Go outside and play! Unless you have to 'work,' like some
-kind of 'responsible adult.' In which case keep a flask in your desk drawer."""
+BODY = """
+The temperature outside is now %d, which is within the target range for MAXIMUM
+ENJOYMENT. Go outside and play! Unless you have to 'work,' like some kind of
+'responsible adult.' In which case keep a flask in your desk drawer.
+"""
+
+MESSAGES = {
+    "entered_optimal": "The weather has entered the optimal range.",
+    "left_optimal": "The weather has taken a turn for the worse.",
+    "still_optimal": "The weather is still really nice.",
+    "still_not_optimal": "The weather is still bad."
+}
 
 # weather configuration
 LOCATION = "Milwaukee"
 TARGET_RANGE = (68, 85)
 TEMPERATURE_RECORD = "last_temperature.txt"
+
 
 def update():
     temperature = get_current_temperature()
@@ -26,30 +39,36 @@ def update():
     if temperature_in_range(temperature):
         # if it just suddenly became a nice day...
         if not temperature_in_range(last_temperature):
-            test_weather_update(temperature, "Entered optimal range.")
+            send_weather_update(temperature, "entered_optimal")
         else:
-            test_weather_update(temperature, "Still in optimal range.")
+            send_weather_update(temperature, "still_optimal")
     else:
         # if it was a nice day and now it's not...
         if temperature_in_range(last_temperature):
-            test_weather_update(temperature, "Left optimal range.")
+            send_weather_update(temperature, "left_optimal")
         else:
-            test_weather_update(temperature, "Still outside optimal range.")
+            send_weather_update(temperature, "still_not_optimal")
 
     save_temperature(temperature)
+
 
 def temperature_in_range(temperature):
     """True if the supplied temperature is within TARGET_RANGE."""
     min, max = TARGET_RANGE
     return number_in_range(temperature, min, max)
 
+
 def get_current_temperature():
     """Return the current temperature, loaded from the Google weather API."""
-    request = "http://www.google.com/ig/api?weather=%s" % LOCATION
-    response = xml.dom.minidom.parse(urlopen(request))
-    temperature_node = find_node(response, "temp_f")
+    if not DEBUG:
+        request = "http://www.google.com/ig/api?weather=%s" % LOCATION
+        response = xml.dom.minidom.parse(urlopen(request))
+        temperature_node = find_node(response, "temp_f")
+        return int(temperature_node.attributes["data"].firstChild.nodeValue)
 
-    return int(temperature_node.attributes["data"].firstChild.nodeValue)
+    else:
+        return int(file("debug_temperature.txt").read())
+
 
 def get_last_temperature():
     """Return the most recently recorded temperature in the log, creating a log
@@ -62,9 +81,11 @@ def get_last_temperature():
             return 0
     else:
         return 0
-    
+
+
 def save_temperature(temperature):
     file(TEMPERATURE_RECORD, "w").write(str(temperature))
+
 
 # utility
 def find_node(root, target_name):
@@ -85,6 +106,7 @@ def find_node(root, target_name):
     else:
         return None
 
+
 def number_in_range(n, min, max):
     """True if n is within the supplied range."""
     if n >= min and n <= max:
@@ -92,29 +114,46 @@ def number_in_range(n, min, max):
     else:
         return False
 
+
 def test_weather_update(temperature, message):
     if not os.path.exists("test.log"):
         log_file = file("test.log", "w")
     else:
         log_file = file("test.log", "a")
 
-    debug_output = "%d degrees, observed %s: %s\n" 
+    debug_output = "%d degrees, observed %s: %s\n"
     timestamp = datetime.now().strftime("%H:%M:%S, %Y-%m-%d")
     log_file.write(debug_output % (temperature, timestamp, message))
 
-def send_weather_update(temperature):
-    try:
-        """Send email with the supplied message."""
-        sendmail_location = "/usr/sbin/sendmail" # sendmail location
-        p = os.popen("%s -t" % sendmail_location, "w")
-        p.write("From: %s\n" % SENDER)
-        p.write("To: %s\n" % ",".join(RECIPIENTS))
-        p.write("Subject: %s\n" % SUBJECT)
-        p.write("\n") # blank line separating headers from body
-        p.write(BODY % temperature)
-        status = p.close()
-        if status != 0:
-            # TODO: maybe some kind of logging later?
+
+def send_weather_update(temperature, situation):
+    """Outside debug mode, send an email if the situation is 'entered_optimal';
+    in debug mode, log every situation to test.log."""
+    if not DEBUG and situation == "entered_optimal":
+        try:
+            """Send email with the supplied message."""
+
+            sendmail_location = "/usr/sbin/sendmail"  # sendmail location
+            p = os.popen("%s -t" % sendmail_location, "w")
+            p.write("From: %s\n" % SENDER)
+            p.write("To: %s\n" % ",".join(RECIPIENTS))
+            p.write("Subject: %s\n" % SUBJECT)
+            p.write("\n")  # blank line separating headers from body
+            p.write(BODY % temperature)
+            status = p.close()
+            if status != 0:
+                # TODO: maybe some kind of logging later?
+                pass
+        except Exception as error:
             pass
-    except Exception as error:
-        pass
+    else:
+        timestamp = datetime.now().strftime("%H:%M:%S, %Y-%m-%d")
+        debug_output = "%d degrees, observed %s: %s\n" % (temperature,
+                                                          timestamp,
+                                                          MESSAGES[situation])
+        print debug_output
+        file("test.log", "a").write(debug_output)
+
+# when run as command-line application
+if __name__ == "__main__":
+    update()

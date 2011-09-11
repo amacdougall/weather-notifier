e4180f40240a59ff272a00e5fe84ddb8b7730e43
amacdougall <smoke@alanmacdougall.com>
2011-09-11 13:46:47 -0500

    notifier.py now queries Google.

--- notifier.py
+++ notifier.py
@@ -7,5 +7,25 @@ LOCATION = "Milwaukee"
 
 def update():
     request = "http://www.google.com/ig/api?weather=%s" % LOCATION
-    response = urllib.urlopen(request).read()
+    response = xml.dom.minidom.parse(urlopen(request))
+    temperature_node = find_node(reponse, "temp_f")
+    temperature = temperature_node.attributes["data"].firstChild.nodeValue;
+    # TODO: do something with the temperature
 
+def find_node(root, target_name):
+    """Starting from the root node, return the first node with the
+    target_name."""
+
+    if root.localName == target_name:
+        return root
+    elif len(root.childNodes) > 0:
+        for node in root.childNodes:
+            if node.localName == target_name:
+                return node
+            elif len(node.childNodes) > 0:
+                found = find_node(node, target_name)
+                if found is not None:
+                    return found
+        return None
+    else:
+        return None
